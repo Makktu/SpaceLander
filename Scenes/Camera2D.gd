3b0e5a6f30bd366694667b2df2d2d84cea335049
Makktu <johnmcna@gmail.com>
2022-09-13 12:02:04 +0100

    added zoom-out at end of level

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -4,6 +4,7 @@ var zoom_start = 1
 var new_zoom = 0.001
 
 func _on_ZoomCameraDetection_body_entered(body):
+	# zooms in at Bottleneck1 (near start)
 	if body.name == "Player":
 		for n in 200: 			
 			zoom = Vector2(zoom_start,zoom_start)
@@ -12,9 +13,19 @@ func _on_ZoomCameraDetection_body_entered(body):
 
 
 func _on_ZoomCameraDetection_body_exited(body):
+	# zooms in at Bottleneck2 (near end)
 	if body.name == "Player":
 		for n in 200: 			
 			zoom = Vector2(zoom_start,zoom_start)
 			zoom_start += new_zoom
 			yield(get_tree().create_timer(0.02), "timeout")
 		zoom = Vector2(1,1)
+
+
+func _on_ZoomCameraDetection3_body_entered(body: Node) -> void:
+	# Zooms OUT at the approach to the portal
+		if body.name == "Player":
+			for n in 650: 			
+				zoom = Vector2(zoom_start,zoom_start)
+				zoom_start += new_zoom
+				yield(get_tree().create_timer(0.02), "timeout")
