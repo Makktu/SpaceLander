0a08ea8c4fbd565e002550acafb04d6d465d347d
Makktu <johnmcna@gmail.com>
2022-11-05 19:25:54 +0000

    privacy policy added

--- Scripts/CreditScroll.gd
+++ Scripts/CreditScroll.gd
@@ -5,3 +5,9 @@ func _physics_process(delta: float) -> void:
 
 func _on_Back_pressed() -> void:
 	get_tree().change_scene("res://Scenes/GameOver.tscn")
+
+
+func _on_PrivacyPolicy_pressed():
+	$CreditsContainer.visible = false
+	$PrivacyPolicy.visible = true
+	
--- Scripts/CreditsListing.gd
+++ Scripts/CreditsListing.gd
@@ -4,3 +4,7 @@ extends Node2D
 func _physics_process(delta: float) -> void:
 	if position.y > -990:
 		position.y -= 0.5
+
+
+func _on_PrivacyPolicy_pressed():
+	pass # Replace with function body.
